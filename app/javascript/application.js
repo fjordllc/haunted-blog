748b8fc2ea4c9c5d43953ded98cd40c864d13a05
Junichi Ito <jit@sonicgarden.jp>
2022-03-19 15:09:35 +0900

    Setup Vue.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import Vue from "vue/dist/vue.esm.browser"
+document.addEventListener('turbo:load', () => {
+  const app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+})
