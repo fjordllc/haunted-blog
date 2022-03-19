6b5fdbd6187bee3ef1099d17344e922b0e01d749
Junichi Ito <jit@sonicgarden.jp>
2022-03-19 16:30:10 +0900

    Require login for blogs#new

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :authenticate_user!, only: %i(edit create update destroy)
+  before_action :authenticate_user!, only: %i(new edit create update destroy)
 
   before_action :set_blog, only: %i[ show edit update destroy ]
 
