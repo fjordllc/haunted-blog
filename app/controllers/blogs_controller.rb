1d26fbc6880db0818ca31e6c42e551abe8aba35e
Junichi Ito <jit@sonicgarden.jp>
2022-03-20 17:07:35 +0900

    Fix vulnerability

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,13 +3,15 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    @blog = Blog.find(params[:id])
+  end
 
   def new
     @blog = Blog.new
@@ -44,10 +46,12 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    fields = %i[title content secret]
+    fields.push(:random_eyecatch) if current_user.premium?
+    params.require(:blog).permit(*fields)
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,7 +10,8 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where("title LIKE '%#{term}%' OR content LIKE '%#{term}%'")
+    escaped_term = sanitize_sql_like(term.to_s)
+    where('title LIKE :term OR content LIKE :term', term: "%#{escaped_term}%")
   }
 
   scope :default_order, -> { order(id: :desc) }
