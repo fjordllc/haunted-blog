5fd22caa068691e27c726900e79e4ddebf255700
akingo55 <am1157154@gmail.com>
2022-10-16 22:16:35 +0900

    fix sql injection

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,7 +10,8 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where("title LIKE '%#{term}%' OR content LIKE '%#{term}%'")
+    joined_term = "%#{term}%"
+    where('title LIKE ? OR content LIKE ?', joined_term, joined_term)
   }
 
   scope :default_order, -> { order(id: :desc) }
