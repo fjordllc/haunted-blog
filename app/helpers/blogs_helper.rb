3b4def6ece685069859613cf43331c14f05afbbe
akingo55 <am1157154@gmail.com>
2022-10-16 22:16:13 +0900

    fix xss

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    blog.content.gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
+    h(blog.content).gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
   end
 end
