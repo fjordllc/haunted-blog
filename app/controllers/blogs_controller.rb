451f1607c36008604cc7a3696035874dd65fd2ad
mono-nobe <ryoya.goemono@gmail.com>
2023-01-13 20:21:13 +0900

    不正アクセス対応 - 他の人の記事を編集できない - 他の人の秘密の記事にアクセスできない

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,13 +3,19 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    @blog = Blog.find(params[:id])
+
+    return unless @blog.secret? && !@blog.owned_by?(current_user)
+
+    raise ActiveRecord::RecordNotFound
+  end
 
   def new
     @blog = Blog.new
@@ -44,7 +50,7 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
