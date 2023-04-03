b6854c32dd60508eb5b384e40aea4193d0a6a142
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-03 21:33:55 +0900

    SQLインジェクションの脆弱性を修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,9 +9,7 @@ class Blog < ApplicationRecord
 
   scope :published, -> { where('secret = FALSE') }
 
-  scope :search, lambda { |term|
-    where("title LIKE '%#{term}%' OR content LIKE '%#{term}%'")
-  }
+  scope :search, ->(term) { where('title LIKE ?', "%#{sanitize_sql_like(term || '')}%").or(where('content LIKE ?', "%#{sanitize_sql_like(term || '')}%")) }
 
   scope :default_order, -> { order(id: :desc) }
 
