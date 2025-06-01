30ab2189cc2b4fc4fc3d0696b70507b08492bdd9
Junichi Ito <jit@sonicgarden.jp>
2025-06-01 17:40:33 +0900

    Removed unused option

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,7 +5,6 @@ require 'test_helper'
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
     options.add_argument('--disable-gpu')
-    options.add_preference('profile.password_manager_leak_detection', false)
   end
 
   def sign_in_as(user)
