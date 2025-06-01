69fa29724735dc441007115fc089fa547ae4de46
Junichi Ito <jit@sonicgarden.jp>
2025-06-01 16:41:02 +0900

    Update to Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_19_210918) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_01_071841) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
