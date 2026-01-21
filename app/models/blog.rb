bd4a01526d539a600466ea943ac0ef2129369368
shoma <s.tone@genbasupport.com>
2026-01-21 16:16:02 +0900

    fix: blog検索欄のSQLインジェクション脆弱性を修正
    動的にSQLを生成していたため、SQLインジェクションによって
    秘密のブログを他のユーザーが見られるようになっていた

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,7 +10,7 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where("title LIKE '%#{term}%' OR content LIKE '%#{term}%'")
+    where('title LIKE :t OR content LIKE :t', t: "%#{term}%")
   }
 
   scope :default_order, -> { order(id: :desc) }
