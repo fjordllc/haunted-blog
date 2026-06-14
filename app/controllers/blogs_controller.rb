b1dded3ea9ca07f97038ad5c08ab208526b1ba5b
okamuratakeru <takeokamura11@gmail.com>
2026-06-14 16:18:24 +0900

    fix/権限昇格対策

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,8 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show]
+  before_action :set_my_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -45,6 +46,10 @@ class BlogsController < ApplicationController
 
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+
+  def set_my_blog
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
