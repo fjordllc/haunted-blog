8e968cd0ee433a80e5a41a240836039fb27643c9
stahnu <shota.horinouchi@gmail.com>
2026-08-27 11:51:58 +0900

    無課金ユーザーが課金機能を使える問題を修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -55,6 +55,9 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.expect(blog: %i[title content secret random_eyecatch])
+    permitted = %i[title content secret]
+    permitted << :random_eyecatch if current_user.premium?
+
+    params.expect(blog: permitted)
   end
 end
